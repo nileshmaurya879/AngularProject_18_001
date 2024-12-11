4b241c87346203e66fa81d3b65a7853fb147dfa0
nilesh <nileshmaurya879@gmail.com>
2024-12-12 00:07:37 +0530

    Added UI form for Category

--- src/app/using-service-category/using-service-category.component.ts
+++ src/app/using-service-category/using-service-category.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { CategoryServiceService } from '../category-service.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-using-service-category',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './using-service-category.component.html',
   styleUrl: './using-service-category.component.scss'
 })
 export class UsingServiceCategoryComponent {
 
-  CategoryTest:any={
-    
+  CategoryTest:any=[]
+  studObj:any = {
+    "CategoryName":"",
+    "CategoryId":0
   }
 
   constructor(private CatSer : CategoryServiceService){
@@ -22,9 +25,27 @@ export class UsingServiceCategoryComponent {
   GetCategoryDataList() {
     this.CatSer.getCategoryList().subscribe(
       (res: any) => {
+        this.CategoryTest = res;
         console.log(res); // Log the response
       }
     );
   }
-  
+  FetchCategory(item:any){
+
+  }
+  DeleteCategory(CatId:any){
+
+  }
+
+  postCategoryData(){
+
+  }
+
+  UpdateCategoryData(){
+
+  }
+
+  ResetCategoryData(){
+    
+  }
 }
